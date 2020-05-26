75415f271a4370fa7afc139ce077f91b2cc3f6be
HemasankarReddy <hemasankar.se@gmail.com>
2020-05-26 22:13:03 +0530

    test

--- src/main/java/com/svs/cloud/smsapp/boot/SmsAppStudentsServiceApplication.java
+++ src/main/java/com/svs/cloud/smsapp/boot/SmsAppStudentsServiceApplication.java
@@ -2,7 +2,7 @@ package com.svs.cloud.smsapp.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
 
